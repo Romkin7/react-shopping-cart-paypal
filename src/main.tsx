eb842d2287b682b6f255068ae64defe30b92dc7e
Roman <devdesign@devdesign.fi>
2023-11-10 21:09:21 +0200

    adds redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {Provider} from 'react-redux';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider>
     <App />
+    </Provider>
   </React.StrictMode>,
 )
---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+
+const store = createStore(() => ({
+  birds: [
+    {
+      name: 'robin',
+      views: 1
+    }
+  ]
+}));
+
+export default store;
