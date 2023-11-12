f941d6ddcfd55f5933c35f82e388123459597405
Swastik <swastikpanda32@gmail.com>
2023-11-12 20:39:05 +0530

    modified brands and categories & added react-router-dom

--- src/@types/product.ts
+++ src/@types/product.ts
@@ -1,5 +1,38 @@
-export type Brands = 'Apple' | 'Samsung' | 'OPPO' | 'Huawei' | 'Infinix';
-export type Categories = 'smartphones' | 'laptops' | 'fragrances';
+export type Brands =
+    | 'Apple'
+    | 'Samsung'
+    | 'OPPO'
+    | 'Huawei'
+    | 'Infinix'
+    | 'Microsoft Surface'
+    | 'HP Pavilion'
+    | 'Impression of Acqua Di Gio'
+    | 'Royal_Mirage'
+    | 'Fog Scent Xpressio'
+    | 'Al Munakh'
+    | 'Lord - Al-Rehab'
+    | "L'Oreal Paris"
+    | 'Hemani Tea'
+    | 'Dermive'
+    | 'ROREC White Rice'
+    | 'Fair & Clear'
+    | 'Saaf & Khaas'
+    | 'Bake Parlor Big'
+    | 'Baking Food Items'
+    | 'fauji'
+    | 'Dry Rose'
+    | 'Boho Decor'
+    | 'Flying Wooden'
+    | 'LED Lights'
+    | 'luxury palace'
+    | 'Golden';
+    export type Categories =
+    | 'smartphones'
+    | 'laptops'
+    | 'fragrances'
+    | 'skincare'
+    | 'groceries'
+    | 'home-decoration';
 
 interface IProduct {
     id: number;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React, { FC } from 'react';
 import './App.scss';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import Cart from './pages/cart';
 import ProductsPage from './pages/products';
+
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element: '/ProductsPage'
+    },
+    {
+        path: '/cart',
+        element: '/CartPage'
+    }
+])
 
 const App: FC = () => {
     return (
         <main>
-            <Cart />
-            <ProductsPage />
+            <RouterProvider router={router} />
         </main>
     );
 };
